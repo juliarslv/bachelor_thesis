ecc048f314272b747dd7b9d855111e09697d8681
roslava <julia.roslava@sulzer.de>
2024-11-13 00:34:46 +0100

    LLAMA2-7B

--- Llama-2-7b.py
+++ Llama-2-7b.py
@@ -10,20 +10,25 @@ Accsess to hugginh face is needed
 
 '''
 
+from transformers import LlamaTokenizer, LlamaModel
 
+# Load the tokenizer and model
 tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+model = LlamaModel.from_pretrained("meta-llama/Llama-2-7b-hf")
 
+# Define the input text
 text = "Hello, my name is Julia."
 
+# Tokenize the input text
 inputs = tokenizer(text, return_tensors="pt")
 input_ids = inputs["input_ids"]
 
+# Get the embeddings
 embedding_layer = model.get_input_embeddings()
 token_embeddings = embedding_layer(input_ids)
 
 print(token_embeddings)
-
-
 print(token_embeddings.shape)
+
 
 
